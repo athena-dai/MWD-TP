3747c4395a22c77cf494df1e7e74f2145aca0f2e
xanderfann <91031450+xanderfann@users.noreply.github.com>
2021-11-26 18:57:15 -0500

    I changed the button sizes so they dont overlap when we shrink the screen. I also added the js file and what I have so far

---
+++ cards.js
@@ -0,0 +1,18 @@
+var elements = document.getElementsByClassName('card'); 
+
+function MouseOver()   {
+   var elements = document.getElementsByClassName('card'); 
+   elements.mouseIsOver = false;
+
+   elements.onmouseover = function()   {
+      this.mouseIsOver = true;
+   };
+   elements.onmouseout = function()   {
+      this.mouseIsOver = false;
+   }
+   elements.onclick = function()   {
+      if (this.mouseIsOver)   {
+         el.className = 'card-white';   
+      }
+   }
+}
