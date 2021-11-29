1fc97218ea59f90612afb47777acf74cb8e391f6
Athena <dai.athena@gmail.com>
2021-11-29 18:18:12 -0500

    info info overlay rough draft code

--- overlay.js
+++ overlay.js
@@ -1,7 +1,11 @@
+// for Map.html
+
+// display filter overlay when filter button is pressed
 function on() {
   document.getElementById("overlay").style.display = "block";
 }
 
+// hide filter overlay otherwise
 function off() {
   document.getElementById("overlay").style.display = "none";
 }
