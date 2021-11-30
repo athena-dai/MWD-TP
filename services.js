dc08bc41255be1172e30c0dfc900e7681a31a282
xanderfann <91031450+xanderfann@users.noreply.github.com>
2021-11-29 20:33:10 -0500

    added overlays ot the service page

--- services.js
+++ services.js
@@ -8,33 +8,35 @@ document.getElementById("advoc-info").addEventListener("click", showInfo, true);
 
 
 function showInfo(ev){
+    document.getElementById("overlay").style.display = "block";
     document.getElementById("info-overlay").style.display = "block";
+
 
     // display different header and supporting text 
     // depending on which card's info icon clicked
     if (ev.currentTarget.id == "school-info"){
         document.getElementById("info-header").innerHTML = "School or Educational Support";
-        document.getElementById("info-text").innerHTML = "School or Educational Support info";
+        document.getElementById("info-text").innerHTML = "Schools that have services that support students with learning disabilities, schools for people with learning disabilities, tutoring centers that help people with learning disabilities";
     }
     else if (ev.currentTarget.id == "support-info"){
         document.getElementById("info-header").innerHTML = "Finding a Support Group";
-        document.getElementById("info-text").innerHTML = "support info";
+        document.getElementById("info-text").innerHTML = "Communities related to learning disabilities that can guide you and provide different resources and advice to deal with learning disabilities";
     }
     else if (ev.currentTarget.id == "accom-info"){
         document.getElementById("info-header").innerHTML = "Learning how to receive accomodations";
-        document.getElementById("info-text").innerHTML = "accomodation info";
+        document.getElementById("info-text").innerHTML = "Communities that teaching how to recieve support and benefits for different areas of life";
     }
     else if (ev.currentTarget.id == "lawyer-info"){
         document.getElementById("info-header").innerHTML = "Finding a Lawyer for Discrimination Claim";
-        document.getElementById("info-text").innerHTML = "lawyer info";
+        document.getElementById("info-text").innerHTML = "Receive legal consultation or legal representation regarding your learning disabilities";
     }
     else if (ev.currentTarget.id == "med-info"){
         document.getElementById("info-header").innerHTML = "Finding a Therapist or Medical Support";
-        document.getElementById("info-text").innerHTML = "therapy info";
+        document.getElementById("info-text").innerHTML = "Seek professional help from psychiatrist, therapist, psychologist or any other medical personnel. This also includes medical or research facilities catered to learning disabilities";
     }
     else if (ev.currentTarget.id == "advoc-info"){
         document.getElementById("info-header").innerHTML = "Learning How to Advocate for Yourself";
-        document.getElementById("info-text").innerHTML = "advocacy info";
+        document.getElementById("info-text").innerHTML = "Find groups that will seek to spread awareness adn representation for communities with learning disabilities";
     }
     
     ev.preventDefault();
@@ -43,4 +45,5 @@ function showInfo(ev){
 
 function hideInfo(){
     document.getElementById("info-overlay").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
 }
