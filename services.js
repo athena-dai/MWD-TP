103753c548531572ee69b231a356b26fe86ea6a1
Athena <dai.athena@gmail.com>
2021-11-29 19:00:13 -0500

    better formatting for info box overlays on services.html page

--- services.js
+++ services.js
@@ -5,8 +5,38 @@ document.getElementById("lawyer-info").addEventListener("click", showInfo, true)
 document.getElementById("med-info").addEventListener("click", showInfo, true);
 document.getElementById("advoc-info").addEventListener("click", showInfo, true);
 
+
+
 function showInfo(ev){
     document.getElementById("info-overlay").style.display = "block";
+
+    // display different header and supporting text 
+    // depending on which card's info icon clicked
+    if (ev.currentTarget.id == "school-info"){
+        document.getElementById("info-header").innerHTML = "School or Educational Support";
+        document.getElementById("info-text").innerHTML = "School or Educational Support info";
+    }
+    else if (ev.currentTarget.id == "support-info"){
+        document.getElementById("info-header").innerHTML = "Finding a Support Group";
+        document.getElementById("info-text").innerHTML = "support info";
+    }
+    else if (ev.currentTarget.id == "accom-info"){
+        document.getElementById("info-header").innerHTML = "Learning how to receive accomodations";
+        document.getElementById("info-text").innerHTML = "accomodation info";
+    }
+    else if (ev.currentTarget.id == "lawyer-info"){
+        document.getElementById("info-header").innerHTML = "Finding a Lawyer for Discrimination Claim";
+        document.getElementById("info-text").innerHTML = "lawyer info";
+    }
+    else if (ev.currentTarget.id == "med-info"){
+        document.getElementById("info-header").innerHTML = "Finding a Therapist or Medical Support";
+        document.getElementById("info-text").innerHTML = "therapy info";
+    }
+    else if (ev.currentTarget.id == "advoc-info"){
+        document.getElementById("info-header").innerHTML = "Learning How to Advocate for Yourself";
+        document.getElementById("info-text").innerHTML = "advocacy info";
+    }
+    
     ev.preventDefault();
     ev.stopPropagation();
 }
